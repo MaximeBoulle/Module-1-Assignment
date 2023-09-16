7608a2c4541fa6dd7998c0adf5fb1d4c33699af6
Maxime Boulle <maxime.boulle1@gmail.com>
2023-09-15 18:04:32 -0700

    All the starter files for the website with NodeJS

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Website ! "Chose it!"
+
+### Description
+
+Our _website_ named **Chose it!** had many **_features_** like *online shop* or *promotions videos*.
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("the first script of our web site!");
+console.log("This function will give you the next drop of the new shoes of Ed's Shoes");
+
+random = Math.floor(Math.random() * 10);
+
+if (random != 0) {
+    console.log("The new shoes are available in " + random + " days");
+}
